d967e71e2e70571e4b8fa4b5494ba1e6e2eb5e92
kanouken <1462927100@qq.com>
2019-06-21 12:07:57 +0800

    Create forgetNode.sh

---
+++ redis/forgetNode.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+flags_noaddr_node_id="f2c9d6445359372bc7efbe16e53cdaa2d7b38923"
+
+ip_port=$(redis-cli  cluster nodes | egrep -v 'noaddr|handshake|fail' | awk '{print $2}')
+
+
+
+for i in $ip_port
+
+do
+
+  eval $(echo $i | awk -F: '{printf("ip=%s;port=%s",$1,$2)}')
+
+  redis-cli -h $ip -p $port cluster forget $flags_noaddr_node_id
+
+
+
+  #flags_noaddr_node_id=$(redis-cli -h $ip -p $port cluster nodes | grep 'noaddr' | awk '{print $1; exit; }')
+
+  #test -n "$flags_noaddr_node_id" && echo $ip, $port, $flags_noaddr_node_id
+
+
+
+done
